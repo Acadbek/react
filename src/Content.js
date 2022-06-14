5ca8717d601e866b2dc1504f6ed7a17cfdcfd05c
Asadbek <a.nosirjonov@uic.group>
2022-06-14 15:34:32 +0500

    other function

--- src/Content.js
+++ src/Content.js
@@ -7,19 +7,23 @@ class App extends Component {
     }
   }
 
-  someFunc = (e) => {
-    e.target.id === 'incre' ? this.setState({ count: this.state.count + 1 })
-      : e.target.id === 'zero' ? this.setState({ count: this.state.count = 0 })
-        : this.setState({ count: this.state.count - 1 })
+  someFunc(val) {
+    this.setState({count: val})
   }
+
+  // someFunc = (e) => {
+  //   e.target.id === 'incre' ? this.setState({ count: this.state.count + 1 })
+  //     : e.target.id === 'zero' ? this.setState({ count: this.state.count = 0 })
+  //       : this.setState({ count: this.state.count - 1 })
+  // }
   render() {
     return (
       <div>
-        <button id="incre" onClick={this.someFunc}> + </button>
+        <button id="incre" onClick={() => this.someFunc(this.state.count + 1)}> + </button>
         <h1>{this.state.count}</h1>
-        <button onClick={this.someFunc}> - </button>
+        <button onClick={() => this.someFunc(this.state.count - 1)}> - </button>
         <br /><br /><br /><br />
-        <button id="zero" onClick={this.someFunc}>0</button>
+        <button id="zero" onClick={() => this.someFunc(0)}>0</button>
       </div>
     )
   }
