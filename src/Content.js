47134c54313911963f604ebc2c955ba7255deea3
Asadbek <a.nosirjonov@uic.group>
2022-06-14 15:15:08 +0500

    incre and decre function

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,33 @@
-import {Component} from "react";
+import { Component, useState } from "react"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
 
-class App extends Component{
+  someFunc = (e) => {
+    if(e.target.id === 'incre') {
+      this.setState({count: this.state.count + 1})
+    }
+    else{
+      this.setState({count: this.state.count - 1})
+    }
+  }
+  zero = () => {
+    this.setState({count: this.state.count = 0})
+  }
+
   render() {
-    return(
-      <div>BYE WORLD</div>
+    return (
+      <div>
+        <button id="incre" onClick={this.someFunc}> + </button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.someFunc}> - </button>
+        <br/><br/><br/><br/>
+        <button onClick={this.zero}>0</button>
+      </div>
     )
   }
 }
