4fa0d08ccfc917528ca2dc28b5c0f92648b3cd9d
Asadbek <a.nosirjonov@uic.group>
2022-06-15 21:22:43 +0500

    state, simple practice

--- src/practiceHooks.js
+++ src/practiceHooks.js
@@ -1,19 +1,40 @@
-import {Component, useState} from "react";
-
+import {Component, useState, setState} from "react";
 class PracticeHooks extends Component{
-  state = {
-    count: 7,
-    stop: false
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      stop: true,
+    }
   }
 
-  componentDidMount() {
-    window.localStorage.getItem('count',  this.setState({count: this.state.count + 1}))
+  start = () => {
+    this.setState({stop: false})
+    this.counterID = setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000)
+  }
+
+  stop = () => {
+    this.setState({stop: true})
+    clearInterval(this.counterID)
+  }
+
+  reset = () => {
+    clearInterval(this.counterID)
+    this.setState({stop: true})
+    this.setState({count: 0})
   }
 
   render() {
     return(
       <div>
-        <button onClick={this.start}>Start</button>
+          { 
+            this.state.stop ? 
+            <button onClick={this.start}>Start</button> :
+            <button onClick={this.stop}>Stop</button>
+          }
+          <button onClick={this.reset}>Reset</button>
         <h1>
           {this.state.count}
         </h1>
